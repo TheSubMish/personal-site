e7f13aa864b98b1ffffc2dbdb4d670e7134c6469
TheSubMish <100989223+TheSubMish@users.noreply.github.com>
2023-04-03 23:11:33 +0545

    Update drum.js

--- js/drum.js
+++ js/drum.js
@@ -14,40 +14,40 @@ document.addEventListener("keypress", function(event) {
 function drumSound(key) {
     switch (key) {
         case "w":
-            var tom1 = new Audio("/sound/tom-1.mp3");
+            var tom1 = new Audio("./sound/tom-1.mp3");
             tom1.play();
             break;
         case "a":
-            var tom2 = new Audio("/sound/tom-2.mp3");
+            var tom2 = new Audio("./sound/tom-2.mp3");
             tom2.play();
             break;
         case "s":
-            var tom3 = new Audio("/sound/tom-3.mp3");
+            var tom3 = new Audio("./sound/tom-3.mp3");
             tom3.play();
             break;
         case "d":
-            var tom4 = new Audio("/sound/tom-4.mp3");
+            var tom4 = new Audio("./sound/tom-4.mp3");
             tom4.play();
             break;
         case "j":
-            var snare = new Audio("/sound/snare.mp3");
+            var snare = new Audio("./sound/snare.mp3");
             snare.play();
             break;
         case "k":
-            var crash = new Audio("/sound/crash.mp3");
+            var crash = new Audio("./sound/crash.mp3");
             crash.play();
             break;
         case "l":
-            var drumRoll = new Audio("/sound/drum-roll.mp3");
+            var drumRoll = new Audio("./sound/drum-roll.mp3");
             drumRoll.play();
             break;
         default:
-            var rick = new Audio("/sound/rick.mp3");
+            var rick = new Audio("./sound/rick.mp3");
             rick.play();
             break;
     }
 }
 
 function buttonAnimation(currentKeys) {
-    document.querySelector("." + currentKeys).setAttribute(".pressed");
+    document.querySelector("." + currentKeys).setAttribute("pressed", "true");
 }
